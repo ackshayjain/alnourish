452129bfc425e781e7105f66e703abb6f34055b9
Ackshay <ackshayjain@gmail.com>
2018-04-10 16:38:42 +0530

    test cul

--- alnourish/userprofile/urls.py
+++ alnourish/userprofile/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^track/$',views.index,name='index'),
     url(r'^track/new/$',views.new_culture,name='new'),
-    url(r'^track/test/$',views.test_culture,name='testc'),
+    url(r'^track/test/$',views.testc,name='testc'),
     url(r'^track/delete/(?P<name>\w+)/$',views.delete_culture,name='delete'),
 
 ]
